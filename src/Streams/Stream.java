bd23949abd45f051b5696cb602ad8842bca83a2e
Anand  Kadale <anand.kadale@techverito.com>
2024-06-13 12:06:55 +0530

    Implemented Lambda

--- src/Streams/Stream.java
+++ src/Streams/Stream.java
@@ -6,6 +6,6 @@ import java.util.*;
 public class Stream {
     public static void main(String[] args) {
         List<Integer> l = Arrays.asList(5,10,20,30,8,7);
-        l.stream().filter(x -> x%2 == 0).filter(x -> x > 10).forEach(System.out::println);
+        l.stream().filter(x -> x%2 == 0).filter(x -> x > 10).forEach(System.out::println);  // Used Lambda Expression Here for better working
     }
 }
